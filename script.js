6d8ecf6174c1c2b37698143f58105088c3b5c4e8
deepakgudi-pixel <japanesen52018@gmail.com>
2024-01-13 00:36:51 +0530

    optimized

---
+++ script.js
@@ -0,0 +1,36 @@
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+
+
+const cards = Array.from(document.querySelector('.card-stack').children);
+
+const tl = gsap.timeline({ 
+    scrollTrigger: {
+        trigger: ".card-container",
+        start: "top-=100 top",
+        end: "center-=450 top",
+        markers: true,
+        scrub: 1,
+    }
+});
+
+tl.to(cards[1], { y: -150 })
+.to(cards[2], { y: -150 }, "<")
+.to(cards[3], { y: -150 }, "<")
+.to(cards[2], { y: -300 })
+.to(cards[3], { y: -300 }, "<")
+.to(cards[3], { y: -450 })
+
+
+
+  
+
+
+
